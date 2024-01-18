47b01c40e630e976598a0b5bbd6f4baff4c2c1d8
Roman Yakovinich <steveflatcher32@gmail.com>
2024-01-18 20:00:19 +0300

    Improve script for previews

--- scripts/sshot.py
+++ scripts/sshot.py
@@ -1,3 +1,7 @@
+# The script for creating 240p previews(thumbnails)
+# Froma JSON data search/"search_data.json" out of videos "rsc/vid"
+# To a "rsc/img/prev" folder 
+
 import json
 from moviepy.editor import VideoFileClip
 import os
@@ -6,8 +10,15 @@ import os
 if not os.path.exists('../rsc/img/prev'):
     os.makedirs('../rsc/img/prev')
 
-# Open the JSON file and load the data
-with open("../rsc/search/search_data.json") as f:
+# Specify the path to the JSON file
+file_path = "../rsc/search/search_data.json"
+
+# Check if the file exists
+if not os.path.exists(file_path):
+    raise FileNotFoundError(f"The file {file_path} does not exist.")
+
+# If the file exists, open it and load the data
+with open(file_path, encoding='utf-8') as f:
     data = json.load(f)
 
 # Iterate over each item in the data
@@ -16,12 +27,23 @@ for item in data:
     for video in item['videos']:
         # Modify the path to the 'vid' directory
         video_file = os.path.join("..", "rsc/vid", video['video'])
-        clip = VideoFileClip(video_file)
-        for timestamp in video['timestamps']:
-            # Get the base name of the video file without extension
-            video_base_name = os.path.splitext(os.path.basename(video_file))[0]
-            # Save a screenshot as a .jpg file in the "prev" folder
-            output_filename = f"{video_base_name}_{int(timestamp)}.jpg"
-            output_path = os.path.join("../rsc/img/prev", output_filename)
-            clip.save_frame(output_path, t=timestamp)
-        clip.close()  # Close the VideoFileClip object after use
+        # Check if the video file exists
+        if os.path.exists(video_file):
+            clip = VideoFileClip(video_file)
+            # Resize the clip to 240p
+            clip_resized = clip.resize(height=240)
+            video_duration = clip.duration
+            for timestamp in video['timestamps']:
+                # Check if timestamp exists
+                if timestamp < video_duration:
+                    # Get the base name of the video file without extension
+                    video_base_name = os.path.splitext(os.path.basename(video_file))[0]
+                    # Save a screenshot as a .jpg file in the "prev" folder
+                    output_filename = f"{video_base_name}_{int(timestamp)}.jpg"
+                    output_path = os.path.join("../rsc/img/prev", output_filename)
+                    clip_resized.save_frame(output_path, t=timestamp)
+                else:
+                    print(f"Timestamp {timestamp} for ideo file {video_file} does not exist.")    
+            clip.close()  # Close the VideoFileClip object after use
+        else:
+            print(f"Video file {video_file} does not exist.")
